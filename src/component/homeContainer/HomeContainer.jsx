398a971157cc0b9ec41c5ad73c04e461eebd9545
unknown <eimdadulhaque@gmail.com>
2022-03-17 16:58:47 +0600

    side shortcut bar added

--- src/component/homeContainer/HomeContainer.jsx
+++ src/component/homeContainer/HomeContainer.jsx
@@ -5,22 +5,19 @@ import Post_input from '../post-input/Post_input';
 import PostCard from '../post-card/PostCard';
 import ActiveList from '../active-list/ActiveList';
 import DayComponent from '../day/DayComponent';
+import Shortcuts from '../shortcuts/Shortcuts';
 
 export default function () {
     return (
         <div className='container-fluid' style={{ height: "90vh" }}>
-            <div className='row d-flex justify-content-center aligent-item-center'>
-                <div style={{ backgroundColor: "#f8f8f8", height: "90vh", overflow: "scroll" }} className='col-3'>
-                    {
-                        [...Array(100).keys()].map(x =>
-                            <p>Hello...</p>
-                        )
-                    }
+            <div className='row row-cus'>
+                <div className='col-3 p-0 m-0'>
+                    <Shortcuts />
                 </div>
                 <div className='col-6'>
                     <PostCard />
                 </div>
-                <div style={{ backgroundColor: "#f8f8f8", height: "90vh" }} className='col-3 p-0 rounded'>
+                <div className='col-3 p-0'>
                     <ActiveList />
                 </div >
             </div>
---
+++ src/component/shortcuts/Shortcuts.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./Shortcuts.css";
+import Img from "../../asset/003.jpg";
+import { AccessAlarm, AccessTime, ArrowDownward, ArrowDropDownCircle, Groups, OndemandVideo, PeopleAlt, Storefront } from '@mui/icons-material';
+
+export default function shortcuts() {
+    return (
+        <div className='shortcuts-container'>
+            <div className='list-container'>
+                <div className='short-list-header'>
+                    <img src={Img} /> <span>Eimdadul Haque</span>
+                </div>
+                <div className='short-list-header'>
+                    <PeopleAlt />
+                    <span>Friends</span>
+                </div>
+                <div className='short-list-header'>
+                    <Groups />
+                    <span>Group</span>
+                </div>
+                <div className='short-list-header'>
+                    <Storefront />
+                    <span>MarketPalce</span>
+                </div>
+                <div className='short-list-header'>
+                    <OndemandVideo />
+                    <span>Watch</span>
+                </div>
+                <div className='short-list-header'>
+                    <AccessTime />
+                    <span>Memories</span>
+                </div>
+                <div className='short-list-header'>
+                    <ArrowDropDownCircle />
+                    <span>Memories</span>
+                </div>
+
+                <div className='short-group-list-container' >
+                    <h6>Your Shortcuts</h6>
+                    <div className='short-group-list'>
+                        <img src={Img} /> <span>Eimdadul Haque</span>
+                    </div>
+                    <div className='short-group-list'>
+                        <img src={Img} /> <span>Eimdadul Haque</span>
+                    </div>
+                    <div className='short-group-list'>
+                        <img src={Img} /> <span>Eimdadul Haque</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
